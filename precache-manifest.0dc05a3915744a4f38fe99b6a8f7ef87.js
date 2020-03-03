a69da4ef006164d475a179a511a364bc0325b632
Emiliano Fernández Hernández <emilianofh02@gmail.com>
2020-03-02 18:34:35 -0700

    Updates

---
+++ precache-manifest.0dc05a3915744a4f38fe99b6a8f7ef87.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ef1d7db1add3f155a40dff4341acc7",
+    "url": "/insomnia/index.html"
+  },
+  {
+    "revision": "a1ea5e38e46886011924",
+    "url": "/insomnia/static/css/main.2451d915.chunk.css"
+  },
+  {
+    "revision": "6a6155b3b488da0a1f39",
+    "url": "/insomnia/static/js/2.410993a3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/insomnia/static/js/2.410993a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ea5e38e46886011924",
+    "url": "/insomnia/static/js/main.287f908b.chunk.js"
+  },
+  {
+    "revision": "f57f65ca7204f63de5d3",
+    "url": "/insomnia/static/js/runtime-main.6919cf61.js"
+  },
+  {
+    "revision": "81ca6456c8ae6d93f5cbc1ce8da92434",
+    "url": "/insomnia/static/media/InsomniaLogo.81ca6456.jpg"
+  },
+  {
+    "revision": "fa7aab560ecbd474fa7eae28a7298597",
+    "url": "/insomnia/static/media/concierto1.fa7aab56.jpg"
+  },
+  {
+    "revision": "417f914bb9fa005057d3889a0a3c3f9e",
+    "url": "/insomnia/static/media/icomoon.417f914b.svg"
+  },
+  {
+    "revision": "f96d4615e5e9c18da0a35026b066f43d",
+    "url": "/insomnia/static/media/icomoon.f96d4615.ttf"
+  },
+  {
+    "revision": "fae9e717a7e1a3812214c8ded97b5e76",
+    "url": "/insomnia/static/media/icomoon.fae9e717.woff"
+  },
+  {
+    "revision": "ff271db3773ebac294f5b8ca5f4582f9",
+    "url": "/insomnia/static/media/icomoon.ff271db3.eot"
+  }
+]);
